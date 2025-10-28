a8d2af720b741bd770e9675b3a744e7d6edc4b20
Akshay Das <63033525+akshay98322@users.noreply.github.com>
2025-10-28 08:32:59 +0530

    clean up

--- app/purchase/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import Purchase
-from .serializers import PurchaseSerializer
-
-class PurchaseViewSet(viewsets.ModelViewSet):
-    queryset = Purchase.objects.all()
-    serializer_class = PurchaseSerializer
